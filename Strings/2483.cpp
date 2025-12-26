2b46f9d4357e5d3d90525856fa4c737eadd18bc9
Vikas Kumar <vikumar162006@gamil.com>
2025-12-26 15:19:09 +0530

    Daily Question 26Dec Min Penalty

---
+++ Strings/2483.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int bestClosingTime(string cus) {
+        int n = cus.size();
+        vector<int> pn(n + 1, 0), py(n + 1, 0);
+
+        for (int i = 0, j = n - 1; i < n; i++, j--) {
+            pn[i + 1] = pn[i] + (cus[i] == 'N');
+            py[j] = py[j + 1] + (cus[j] == 'Y');
+        }
+
+        int ans = 0, minPenalty = INT_MAX;
+
+        for (int i = 0; i <= n; i++) {
+            int penalty = pn[i] + py[i];
+            if (penalty < minPenalty) {
+                minPenalty = penalty;
+                ans = i;
+            }
+        }
+
+        return ans;
+    }
+};
